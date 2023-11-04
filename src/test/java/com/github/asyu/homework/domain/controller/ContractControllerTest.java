bc67f6dbfde631e19c44c8877d4a3c75b88b1e3f
YuAnsang <kahlman@naver.com>
2023-11-04 22:47:04 +0900

    docs: Rest docs를 이용한 API 명세 추가

--- src/test/java/com/github/asyu/homework/domain/controller/ContractControllerTest.java
+++ src/test/java/com/github/asyu/homework/domain/controller/ContractControllerTest.java
@@ -1,9 +1,16 @@
 package com.github.asyu.homework.domain.controller;
 
 import static com.github.asyu.homework.common.SpringProfiles.TEST;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,8 +24,10 @@ import com.github.asyu.homework.domain.persistence.repository.CoverageRepository
 import com.github.asyu.homework.domain.utils.PremiumCalculator;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,9 +35,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
@@ -42,6 +53,7 @@ import org.springframework.test.web.servlet.MockMvc;
     }
 )
 @ActiveProfiles(TEST)
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc
 @SpringBootTest
 class ContractControllerTest {
@@ -81,6 +93,30 @@ class ContractControllerTest {
         .andExpect(jsonPath("$.product.id").value(request.productId()))
         .andExpect(jsonPath("$.coverages[0].id").value(request.coverageIds().get(0)))
         .andExpect(jsonPath("$.coverages[1].id").value(request.coverageIds().get(1)))
+        .andDo(document("contract/save-contract",
+            requestFields(
+                fieldWithPath("startDate").type(JsonFieldType.STRING).description("계약 시작일").attributes(key("constraint").value("오늘 또는 오늘 이후")),
+                fieldWithPath("durationInMonths").type(JsonFieldType.NUMBER).description("계약 기간(월 단위)").attributes(key("constraint").value("Positive number")),
+                fieldWithPath("productId").type(JsonFieldType.NUMBER).description("상품 ID"),
+                fieldWithPath("coverageIds").type(JsonFieldType.ARRAY).description("담보 ID 리스트").attributes(key("constraint").value("상품 하위에 존재하는 담보만 가능"))
+            ),
+            responseFields(
+                fieldWithPath("id").type(JsonFieldType.NUMBER).description("생성된 계약 ID"),
+                fieldWithPath("startDate").type(JsonFieldType.STRING).description("계약 시작일"),
+                fieldWithPath("endDate").type(JsonFieldType.STRING).description("계약 종료일"),
+                fieldWithPath("durationInMonths").type(JsonFieldType.NUMBER).description("계약 기간(월 단위)"),
+                fieldWithPath("totalPremium").type(JsonFieldType.NUMBER).description("총 보험료"),
+                fieldWithPath("status").type(JsonFieldType.STRING).description("계약 상태").attributes(key("constraint").value(contractStatusField())),
+                fieldWithPath("product").type(JsonFieldType.OBJECT).description("상품 정보"),
+                fieldWithPath("product.id").type(JsonFieldType.NUMBER).description("상품 ID"),
+                fieldWithPath("product.name").type(JsonFieldType.STRING).description("상품명"),
+                fieldWithPath("coverages").description("담보 정보 리스트"),
+                fieldWithPath("coverages[].id").type(JsonFieldType.NUMBER).description("담보 ID"),
+                fieldWithPath("coverages[].name").type(JsonFieldType.STRING).description("담보명"),
+                fieldWithPath("coverages[].insuredAmount").type(JsonFieldType.NUMBER).description("가입 금액"),
+                fieldWithPath("coverages[].baseAmount").type(JsonFieldType.NUMBER).description("기준 금")
+            )
+        ));
     ;
   }
 
@@ -98,6 +134,13 @@ class ContractControllerTest {
         )
         .andExpect(status().isBadRequest())
         .andExpect(jsonPath("$.code").value(ErrorCode.INVALID_PARAMETER.getCode()))
+        .andDo(document("error/errors-400-1",
+            responseFields(
+                fieldWithPath("code").type(JsonFieldType.STRING).description("에러 코드"),
+                fieldWithPath("message").type(JsonFieldType.STRING).description("에러 메시지"),
+                fieldWithPath("cause").type(JsonFieldType.STRING).description("에러 원인")
+            )
+        ));
     ;
   }
 
@@ -125,14 +168,21 @@ class ContractControllerTest {
         )
         .andExpect(status().is4xxClientError())
         .andExpect(jsonPath("$.code").value(expected.getCode()))
+        .andDo(document("error/errors-400-2",
+            responseFields(
+                fieldWithPath("code").type(JsonFieldType.STRING).description("에러 코드"),
+                fieldWithPath("message").type(JsonFieldType.STRING).description("에러 메시지"),
+                fieldWithPath("cause").type(JsonFieldType.STRING).description("에러 원인")
+            )
+        ));
     ;
   }
 
   private static Stream<Arguments> save_failure_cause_invalid_parameter_in_service() {
     return Stream.of(
+        Arguments.of("Not Exists Product", new Post(LocalDate.now(), 3, 3L, List.of(5L)), ErrorCode.NOT_FOUND),
         Arguments.of("Duration out of range", new Post(LocalDate.now(), 4, 1L, List.of(1L, 2L)), ErrorCode.INVALID_PARAMETER_BUSINESS),
-        Arguments.of("Invalid Coverage Ids -> Not Exists In Product", new Post(LocalDate.now(), 3, 1L, List.of(3L)), ErrorCode.INVALID_PARAMETER_BUSINESS),
-        Arguments.of("Not Exists Product", new Post(LocalDate.now(), 3, 3L, List.of(5L)), ErrorCode.NOT_FOUND)
+        Arguments.of("Invalid Coverage Ids -> Not Exists In Product", new Post(LocalDate.now(), 3, 1L, List.of(3L)), ErrorCode.INVALID_PARAMETER_BUSINESS)
     );
   }
 
@@ -154,6 +204,27 @@ class ContractControllerTest {
         .andExpect(jsonPath("$.product.id").value(1))
         .andExpect(jsonPath("$.coverages[0].id").value(1))
         .andExpect(jsonPath("$.coverages[1].id").value(2))
+        .andDo(document("contract/get-contract",
+            pathParameters(
+                parameterWithName("id").description("계약 ID")
+            ),
+            responseFields(
+                fieldWithPath("id").type(JsonFieldType.NUMBER).description("계약 ID"),
+                fieldWithPath("startDate").type(JsonFieldType.STRING).description("계약 시작일"),
+                fieldWithPath("endDate").type(JsonFieldType.STRING).description("계약 종료일"),
+                fieldWithPath("durationInMonths").type(JsonFieldType.NUMBER).description("계약 기간(월 단위)"),
+                fieldWithPath("totalPremium").type(JsonFieldType.NUMBER).description("총 보험료"),
+                fieldWithPath("status").type(JsonFieldType.STRING).description("계약 상태").attributes(key("constraint").value(contractStatusField())),
+                fieldWithPath("product").type(JsonFieldType.OBJECT).description("상품 정보"),
+                fieldWithPath("product.id").type(JsonFieldType.NUMBER).description("상품 ID"),
+                fieldWithPath("product.name").type(JsonFieldType.STRING).description("상품명"),
+                fieldWithPath("coverages").description("담보 정보 리스트"),
+                fieldWithPath("coverages[].id").type(JsonFieldType.NUMBER).description("담보 ID"),
+                fieldWithPath("coverages[].name").type(JsonFieldType.STRING).description("담보명"),
+                fieldWithPath("coverages[].insuredAmount").type(JsonFieldType.NUMBER).description("가입 금액"),
+                fieldWithPath("coverages[].baseAmount").type(JsonFieldType.NUMBER).description("기준 금")
+            )
+        ));
     ;
   }
 
@@ -168,6 +239,13 @@ class ContractControllerTest {
     mockMvc.perform(get("/apis/v1/contracts/{id}", id))
         .andExpect(status().isNotFound())
         .andExpect(jsonPath("$.code").value(ErrorCode.NOT_FOUND.getCode()))
+        .andDo(document("error/errors-404",
+            responseFields(
+                fieldWithPath("code").type(JsonFieldType.STRING).description("에러 코드"),
+                fieldWithPath("message").type(JsonFieldType.STRING).description("에러 메시지"),
+                fieldWithPath("cause").type(JsonFieldType.STRING).description("에러 원인")
+            )
+        ));
     ;
   }
 
@@ -199,6 +277,32 @@ class ContractControllerTest {
         .andExpect(jsonPath("$.product.id").value(1L))
         .andExpect(jsonPath("$.coverages.size()").value(1))
         .andExpect(jsonPath("$.coverages[0].id").value(request.coverageIds().get(0)))
+        .andDo(document("contract/patch-contract",
+            pathParameters(
+                parameterWithName("id").description("계약 ID")
+            ),
+            requestFields(
+                fieldWithPath("durationInMonths").type(JsonFieldType.NUMBER).description("계약 기간(월 단위)").attributes(key("constraint").value("Positive number")),
+                fieldWithPath("coverageIds").type(JsonFieldType.ARRAY).description("담보 ID 리스트").attributes(key("constraint").value("계약 생성 당시의 상품 하위에 존재하는 담보만 가능")),
+                fieldWithPath("status").type(JsonFieldType.STRING).description("계약 상태").attributes(key("constraint").value(contractStatusField()))
+            ),
+            responseFields(
+                fieldWithPath("id").type(JsonFieldType.NUMBER).description("계약 ID"),
+                fieldWithPath("startDate").type(JsonFieldType.STRING).description("계약 시작일"),
+                fieldWithPath("endDate").type(JsonFieldType.STRING).description("계약 종료일"),
+                fieldWithPath("durationInMonths").type(JsonFieldType.NUMBER).description("계약 기간(월 단위)"),
+                fieldWithPath("totalPremium").type(JsonFieldType.NUMBER).description("총 보험료"),
+                fieldWithPath("status").type(JsonFieldType.STRING).description("계약 상태").attributes(key("constraint").value(contractStatusField())),
+                fieldWithPath("product").type(JsonFieldType.OBJECT).description("상품 정보"),
+                fieldWithPath("product.id").type(JsonFieldType.NUMBER).description("상품 ID"),
+                fieldWithPath("product.name").type(JsonFieldType.STRING).description("상품명"),
+                fieldWithPath("coverages").description("담보 정보 리스트"),
+                fieldWithPath("coverages[].id").type(JsonFieldType.NUMBER).description("담보 ID"),
+                fieldWithPath("coverages[].name").type(JsonFieldType.STRING).description("담보명"),
+                fieldWithPath("coverages[].insuredAmount").type(JsonFieldType.NUMBER).description("가입 금액"),
+                fieldWithPath("coverages[].baseAmount").type(JsonFieldType.NUMBER).description("기준 금")
+            )
+        ));
     ;
   }
 
@@ -279,6 +383,16 @@ class ContractControllerTest {
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.totalPremium").exists())
         .andExpect(jsonPath("$.totalPremium").value(expectedTotalPremium.doubleValue()))
+        .andDo(document("contract/get-expected-total-premium",
+            requestFields(
+                fieldWithPath("durationInMonths").type(JsonFieldType.NUMBER).description("계약 기간(월 단위)").attributes(key("constraint").value("Positive number")),
+                fieldWithPath("productId").type(JsonFieldType.NUMBER).description("상품 ID"),
+                fieldWithPath("coverageIds").type(JsonFieldType.ARRAY).description("담보 ID 리스트").attributes(key("constraint").value("상품 하위에 존재하는 담보만 가능"))
+            ),
+            responseFields(
+                fieldWithPath("totalPremium").type(JsonFieldType.NUMBER).description("총 보험료")
+            )
+        ));
     ;
   }
 
@@ -332,6 +446,12 @@ class ContractControllerTest {
         Arguments.of("Invalid Coverage Ids -> Not Exists In Product", new ContractDto.TotalPremiumCriteria(3, 1L, List.of(3L)), ErrorCode.INVALID_PARAMETER_BUSINESS),
         Arguments.of("Not Exists Product", new ContractDto.TotalPremiumCriteria(3, 99L, List.of(1L)), ErrorCode.NOT_FOUND)
     );
+  }
+
+  private String contractStatusField() {
+    return Arrays.stream(ContractStatus.values())
+        .map(type -> String.format("`%s`", type.name()))
+        .collect(Collectors.joining(", "));
   }
 
 }
